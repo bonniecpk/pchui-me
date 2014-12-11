9339d1145c1161ceaf64002ec0020180e3fa446c
Po Ki Chui <pchui@flatworldknowledge.com>
2014-12-11 18:06:35 -0500

    Able to load compass library in sass Added two psd logo templates

--- app.rb
+++ app.rb
@@ -1,18 +1,21 @@
 require "sinatra/base"
 require "haml"
+require "sass"
+require "compass"
 require 'sprockets'
 require 'sprockets-helpers'
+require 'sprockets-sass'
 
 class PChui < Sinatra::Base
   set :sprockets,     Sprockets::Environment.new(root)
-  set :precompile,    [ /\w+\.(js|css).+$/ ]
+  #set :precompile,    [ /\w+\.(js|css).+$/ ]
   set :assets_prefix, "/assets"
-  set :digest_assets, false
-  set(:assets_path)   { assets_prefix }
+  #set :digest_assets, false
+  set :assets_path,   assets_prefix
   
   configure do
     # Setup Sprockets
-    sprockets.append_path File.join(root, 'assets', 'styles')
+    sprockets.append_path File.join(root, 'assets', 'stylesheets')
     sprockets.append_path File.join(root, 'assets', 'js')
     sprockets.append_path File.join(root, 'assets', 'images')
     sprockets.append_path File.join(root, 'assets', 'fonts')
@@ -20,19 +23,31 @@ class PChui < Sinatra::Base
     # Configure Sprockets::Helpers (if necessary)
     Sprockets::Helpers.configure do |config|
       config.environment = sprockets
-      config.prefix      = assets_prefix
-      config.digest      = digest_assets
-      config.public_path = public_folder
+      #config.prefix      = assets_prefix
+      #config.digest      = digest_assets
+      #config.public_path = public_folder
 
       # Force to debug mode in development mode
       # Debug mode automatically sets
       # expand = true, digest = false, manifest = false
       config.debug       = true if development?
     end
+
+    Compass.configuration do |config|
+      config.project_path = File.dirname(__FILE__)
+      config.sass_dir     = '/assets/stylesheets'
+
+      set :haml, { format: :html5 }
+      set :sass, Compass.sass_engine_options
+    end
   end
  
   helpers do
     include Sprockets::Helpers
+  end
+
+  get "/" do
+    haml :index
   end
 
   get "/samples/full_height" do
